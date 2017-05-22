ff5acc39a7e301d6b258bbc61750562deaded9b8
[name]paul <paul.b.sheldon@gmail.com>
2017-05-22 11:47:01 -0400

    pauls component

---
+++ project3test/src/components/Ptest.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Paul extends Component {
+  render() {
+    return (
+      <div className="Paul">
+      </div>
+    );
+  }
+}
+
+export default Paul;
