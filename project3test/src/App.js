22dd5bd440f66eb86cb1c7c24d7a8b4a07dbb8ab
bunmi alo <bunmialo@yahoo.com>
2017-05-22 11:56:41 -0400

    updated Btest

--- project3test/src/App.js
+++ project3test/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Paul from './components/Ptest';
+import Btest from './Component/Btest';
 
 class App extends Component {
   render() {
---
+++ project3test/src/components/Btest.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Btest extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is a test</p>
+      </div>
+    )
+}
+
+export default Btest;
