a08ddd8c970f3b024818c05c376063f3636edeb8
bunmi alo <bunmialo@yahoo.com>
2017-05-22 12:12:39 -0400

    updated Btest

---
+++ project3test/src/components/Gtest.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Gtest extends Component {
+  render() {
+    return (
+      <header>
+        <h2>Gayathri</h2>
+        
+      </header>
+    )
+  }
+}
+
+export default Gtest;
